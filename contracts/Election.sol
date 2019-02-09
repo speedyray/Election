d65adc199b7cdb09e06fe11c631d2efe08a50659
Raynold <speedyray2ray@gmail.com>
2019-02-08 22:02:05 -0500

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+	//Store the Candidate
+	// Read the Candidate
+	string public candidate;
+	// constructor
+	constructor() public{
+      candidate = "Candidate 1";
+	}
+}
